1e3ce32cd99219fe85f49d963585b225555e3f85
David Lains <dlains@gmail.com>
2018-10-06 11:29:59 -0700

    Fixed issues with clox string chapter code.

--- src/object.c
+++ src/object.c
@@ -18,6 +18,14 @@ static Obj *allocateObject(size_t size, ObjType type) {
   return object;
 }
 
+static ObjString *allocateString(char *chars, int length) {
+  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
+  string->length = length;
+  string->chars  = chars;
+
+  return string;
+}
+
 ObjString *copyString(const char *chars, int length) {
   char *heapChars = ALLOCATE(char, length + 1);
   memcpy(heapChars, chars, length);
@@ -36,12 +44,4 @@ void printObject(Value value) {
 
 ObjString *takeString(char *chars, int length) {
   return allocateString(chars, length);
-}
-
-static ObjString *allocateString(char *chars, int length) {
-  ObjString* string = ALLOCATE_OBJ(ObjString, OBJ_STRING);
-  string->length = length;
-  string->chars  = chars;
-
-  return string;
 }
